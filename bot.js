deb714fc88ef8e23394ee43c16edbe5db493a170
Mark <mark.aaron.fisher@gmail.com>
2017-07-13 10:37:16 -0700

    initial commit

---
+++ bot.js
@@ -0,0 +1,14 @@
+console.log("bot is working");
+
+var Twit = require('twit');
+var config = require('./config');
+
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
+
+var twitObj = new Twit(config);

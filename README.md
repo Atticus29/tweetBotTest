36385c429c4eb0e6a132b1da2eacf3594f501c0c
Mark <mark.aaron.fisher@gmail.com>
2017-07-13 11:17:46 -0700

    add README detailing config file set up and filtered for a tweet to my tweetbot

---
+++ README.md
@@ -0,0 +1,10 @@
+Create a config.js file.
+
+`module.exports = {
+
+    consumer_key:         'YOURSTUFFHERE',
+    consumer_secret:      'YOURSTUFFHERE',
+    access_token:         'YOURSTUFFHERE',
+    access_token_secret:  'YOURSTUFFHERE',
+    timeout_ms:           60*1000
+}`
--- bot.js
+++ bot.js
@@ -3,12 +3,18 @@ console.log("bot is working");
 var Twit = require('twit');
 var config = require('./config');
 
-var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-});
-
 var twitObj = new Twit(config);
+
+var params = {
+  q:'@wildmetweetbot',
+  count: 10
+};
+twitObj.get('search/tweets', params, onResult);
+
+function onResult(err, data, response){
+  var tweets = data.statuses;
+  // console.log(tweets);
+  for(var i = 0; i<tweets.length; i++){
+    console.log(tweets[i].text);
+  }
+}
